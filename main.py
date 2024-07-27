0536df1b39a54e7f33c58a9412a90efbd8e82e55
zelpold <67209463+zelpold@users.noreply.github.com>
2024-07-28 02:10:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def create_video_opencv(message, width=100, height=100, fps=24, time=3):
     font = ImageFont.truetype("Uni_Sans_Heavy.otf", height-height//4)
     for t in range(fps * time):
         img_with_text = Image.new(mode="RGB", size=(width, height), color=(209, 123, 193))
-        x -= (font.getlength(message)+20)//(fps * time)
+        x -= (font.getlength(message)+font.getlength(message[0]))/(fps * time)
         draw = ImageDraw.Draw(img_with_text)
         draw.text((x, y), message, (255, 255, 255), font=font)
         frame = cv2.cvtColor(np.array(img_with_text), cv2.COLOR_RGB2BGR)
